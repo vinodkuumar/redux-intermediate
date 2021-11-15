6bf9407501163f17bc6c7379cdbb29064e13f223
VinodKumar Muppera <vinod.muppera@vedantu.com>
2021-11-15 15:12:23 +0530

    redux using hooks

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,51 +1,57 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import { getArtists,artistListSearch } from '.././store/actions';
-import {Link} from 'react-router-dom';
+import React, { useEffect } from 'react';
 
-class Home extends Component {
-    componentDidMount() {
-        this.props.dispatch(getArtists())
-    }
-    showArtistsAll = (data) => (
+import { useSelector, useDispatch } from 'react-redux';
+import { getArtists, artistListSearch } from '.././store/actions';
+import { Link } from 'react-router-dom';
+
+const Home = (props) => {
+    const artistList = useSelector((state) => state.artists.artistList);
+    console.log('artist list = ', artistList);
+    const dispatch = useDispatch();
+    // componentDidMount() {
+    //     this.props.dispatch(getArtists())
+    // }
+    useEffect(() => {
+        dispatch(getArtists());
+    }, [])
+    const showArtistsAll = (data) => (
         data ? (
             data.map((item) => (
-                <Link 
-                    to={`/artist/${item.id}`} 
+                <Link
+                    to={`/artist/${item.id}`}
                     key={item.id}
                     className="artist_item">
-                        <div className="cover" style={{background: `url(/images/${item.cover})`}}>
-                            <div>{item.name}</div>
-                        </div>
-                    </Link>
-               ))
+                    <div className="cover" style={{ background: `url(/images/${item.cover})` }}>
+                        <div>{item.name}</div>
+                    </div>
+                </Link>
+            ))
         ) : null
 
     )
-    getKeywords = (event) => {
+    const getKeywords = (event) => {
         let key = event.target.value
-        this.props.dispatch(artistListSearch(key));
+        dispatch(artistListSearch(key));
     }
-    render() {
-        console.log(this.props)
-        return (
-            <>
-                <div className="search_container">
-                    <h2>Search your Artist</h2>
-                    <input type="text" onChange={event => this.getKeywords(event)}/>
-                </div>
-                <div className="artist_container">
-                    {this.showArtistsAll(this.props.artists.artistList)}
-                </div>
-            </>
-        )
-    }
+    
+    return (
+        <>
+            <div className="search_container">
+                <h2>Search your Artist</h2>
+                <input type="text" onChange={event => getKeywords(event)} />
+            </div>
+            <div className="artist_container">
+                {showArtistsAll(artistList)}
+            </div>
+        </>
+    )
 }
 
-const mapStateToProps = (state) => {
-    return {
-        artists: state.artists
-    }
-}
 
-export default connect(mapStateToProps, null)(Home);
+// const mapStateToProps = (state) => {
+//     return {
+//         artists: state.artists
+//     }
+// }
+
+export default Home;
